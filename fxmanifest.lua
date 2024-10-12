4a73fea84d3a2226ff703e7ad6748417f0ec73b0
Francesco Mapping <francescobirp@gmail.com>
2024-10-12 20:15:03 +0200

    Adding Version
    https://discord.gg/hvNsZ6ajcX

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "bodacious"
+game "gta5"
+
+author "Francesco - discord: francesco441"
+description "MLO - BILGECO https://discord.gg/hvNsZ6ajcX "
+version "1.0.0"
+
+
+this_is_a_map "yes"
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/beverly_metadata_001.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/palazzo_ciope.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/rhpd_props.ytyp'
+
+
+lua54 'yes'
